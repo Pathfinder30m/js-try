017560a661ac0c11d54612004afb8c280e304e2f
Pathfinder30m <mykhaylik@gmail.com>
2022-07-26 12:09:19 +0300

    final version without makeup

--- script2.js
+++ script2.js
@@ -1,26 +1,16 @@
 import disArray from './info.json' assert { type: "json" };
 console.log(disArray);
 
+
 var value = document.getElementById('number');
-value.addEventListener('change', function handleChange(){
-    console.log(value.value)
-    return(value.value);
-});
+
 const optFrom = '<option class="optionFrom">From</option';
 const optTo = '<option class="optionTo">To</option';
 const convFrom = document.getElementById('convertFrom');
 const convTo =   document.getElementById('convertTo');
-convFrom.addEventListener('change', function handleChange(){
-    console.log(convertFrom.value);
-    return(convertFrom.value);
-});
-convTo.addEventListener('change', function handleChange(){
-    console.log(convertTo.value);
-    return(convertTo.value);
-}); 
-var a = parseFloat(convertFrom.value);
-var b = parseFloat(value.value);
-var c = parseFloat(convertTo.value);
+
+
+
 function addElementFrom() {
     convFrom.insertAdjacentHTML('beforeend',optFrom);
 };
@@ -40,16 +30,39 @@ var allOptTo = document.querySelectorAll("option.optionTo");
 console.log(allOptTo);
 for (let i=0; i < allOptFrom.length; i++){
     allOptFrom[i].innerHTML = disArray.distances[i].name;
-    allOptFrom[i].setAttribute('value', disArray.distances[i].value)
+    allOptFrom[i].setAttribute('id','from_'+i);
+    allOptFrom[i].setAttribute('value', parseFloat(disArray.distances[i].value))
 };
-console.log(convFrom.value)
 for (let i=0; i < allOptTo.length; i++){
     allOptTo[i].innerHTML = disArray.distances[i].name;
-    allOptTo[i].setAttribute('value', disArray.distances[i].value)
+    allOptTo[i].setAttribute('id','to_'+i);
+    allOptTo[i].setAttribute('value', parseFloat(disArray.distances[i].value))
 };
+var allOptFromArray = Array.from(allOptFrom);
+var allOptToArray = Array.from(allOptTo);
+console.log(allOptFromArray);
+console.log(allOptToArray);
+
 //var solve = a*b/c;
 function solve() {
+    value.addEventListener('click', function handleChange(){
+        console.log(value.value)
+        return(value.value);
+        
+    });
+    convFrom.addEventListener('click', function handleChange(){
+        console.log(convertFrom.value);
+        return(convertFrom.value);
+    }); 
+    convTo.addEventListener('change', function handleChange(){
+        console.log(convertTo.value);
+        return(convertTo.value);
+    }); 
+    var a = parseFloat(convertFrom.value);
+    var b = parseFloat(value.value);
+    var c = parseFloat(convertTo.value);
     var result = a*b/c;
+    result = result.toFixed(3);
     document.getElementById('result').innerHTML=result
     console.log(result)
 }
